9bbb50d7ed5140dbd14c6bfad1c9bfe196f666e3
Marcin Jabłoński <nnxcomputing@gmail.com>
2025-01-30 21:43:56 +0100

    [WOW64] Implement wow64.dll loading in NTDLL Ldr

--- dll/win32/kernel32/client/proc.c
+++ dll/win32/kernel32/client/proc.c
@@ -3627,6 +3627,12 @@ StartScan:
         }
     }
 
+#define _WOW64
+#if defined(_WIN64) && defined(_WOW64)
+    /* Allow x86_32 */
+    if (ImageInformation.Machine == IMAGE_FILE_MACHINE_I386) {}
+    else
+#endif
     /* Make sure the image was compiled for this processor */
     if ((ImageInformation.Machine < SharedUserData->ImageNumberLow) ||
         (ImageInformation.Machine > SharedUserData->ImageNumberHigh))
